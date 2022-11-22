f4ea91f3c6de753bee1e997f00148e15825521b3
igor-gabs <igor.mendes@sptech.school>
2022-11-22 16:30:02 -0300

    Funcionando API

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "indi",
-    user: "root",
-    password: "AnJiGlM16102021",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
